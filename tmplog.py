9e5c3a37a491982b4d5746d6ec7f6733dcd30f46
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2020-12-24 16:31:35 +0300

    Separate args and kwargs in output if both exist.

--- tmplog.py
+++ tmplog.py
@@ -30,6 +30,8 @@ def tmplog(*args, file=None, context='nlf', format_value=True, **kwargs):
         if output:
             output += ': '
         output += ', '.join(pformat(v) if format_value else v for v in args)
+        if output and len(kwargs.keys()) > 0:
+            output += '; '
         output += ', '.join(
             '{}={}'.format(k, pformat(v) if format_value else v) for k, v in kwargs.items()
         )
